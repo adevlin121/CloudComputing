238b1d0a0e383e9964a695cf1447f8cb1337c382
Aidan Devlin <adevlin121@hotmail.com>
2015-10-14 15:59:14 +0000

    finished lab

--- lab4/lab5/my_application/server.py
+++ lab4/lab5/my_application/server.py
@@ -1,9 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello():
-    return "Hello World!"
+    return "Hello World!\n"
+
+@app.route("/")
+def index():
+    return "Index file"
+
+@app.route("/user/paul")
+def paul():
+    return "User Paul"
+
+@app.route("/post/80")
+def post():
+    return "Post 80"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=8080, debug=True)
